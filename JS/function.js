54379a376b8421bcbbe8606e231d742927f82456
Nikhil Kumawat <personalnikhil5@gmail.com>
2026-05-11 09:18:13 +0530

    Added JS for adding bookmark with proper validation

--- JS/function.js
+++ JS/function.js
@@ -2,29 +2,58 @@
 
 import * as DOM from "./dom.js";
 
-// This function adds bookmark
-export function addBookmark () {
+// This function raises inavlid input error
+export function checkInputValidation () {
 
     let name = DOM.bookmarkName.value;
     let url = DOM.bookmarkUrl.value;
 
-    let checkBookmarkAddingValidity = [];
+    // If all 2 are true then only input feilds are valid
+    let checkValidity = [false , false];
 
+    // Resetting the error state of all inputs
+    DOM.inputFields.forEach((val , idx , arr) => {
+        val.classList.remove("emptyFeild");
+        val.offsetWidth ;
+    });
+
+    // Checking validity & showing error msg 
     if(name === ""){
-        DOM.inputFields[0].classList.toggle("emptyFeild");
-    }
-    if(url === ""){
-        DOM.inputFields[1].classList.toggle("emptyFeild");
-    }
-    if(!isValidUrl(url)){
-        DOM.inputFields[1].classList.toggle("emptyFeild");
-        url.value = "";
+        // Showing Error
+        DOM.inputFields[0].classList.add("emptyFeild");
+    }else {
+        checkValidity.splice(0 , 1 , true);
     }
 
-    DOM.bookmarkConatiner.innerHTML += `
-    <div class="bookmarkItemCard">
-        <a class="bookmarkLink" href="${url}" target="_main">${name}</a>
-        <button class="removeBookmarkButton">Remove</button>
-    </div>
-    `
+    if(url === ""){
+        // Showing Error
+        DOM.inputFields[1].classList.add("emptyFeild");
+    }else {
+        checkValidity.splice(1 , 1 , true);
+    }
+
+    // Checking Validity & Returning 
+    // So we know should the bookmark be created or not 
+    let validity = checkValidity.every((val , idx , arr) => val === true);
+
+    return validity;
+}
+
+// This function adds bookmark
+export function addBookmark () {
+
+    let validity = checkInputValidation();
+
+    if(validity){
+
+        let name = DOM.bookmarkName.value;
+        let url = DOM.bookmarkUrl.value;
+
+        DOM.bookmarkConatiner.insertAdjacentHTML("beforeend" , 
+            `<div class="bookmarkItemCard">
+                <a class="bookmarkLink" href="${url}" target="_main">${name}</a>
+                <button class="removeBookmarkButton">Remove</button>
+            </div>`
+        );
+    }
 }
--- JS/script.js
+++ JS/script.js
@@ -6,4 +6,21 @@ import * as fun from "./function.js";
 // This eventlistener adds bookmarks on click
 DOM.addBookmarkBtn.addEventListener("click" , () => {
     fun.addBookmark();
+    console.log("Click !!");
+
+    // Only if both fiels are not empty , we should reset th einput feilds
+    // Because Thats the only situation where actually a book mark is created
+    if(DOM.bookmarkName.value !== "" && DOM.bookmarkUrl.value !== ""){
+        DOM.bookmarkName.value = "";
+        DOM.bookmarkUrl.value = "";
+    }
+
+    
+});
+
+// If The user starts to type in any feild it needs to be reset instantaneously
+DOM.inputFields.forEach((val , idx , arr) => {
+    val.addEventListener("input" , () => {
+        val.classList.remove("emptyFeild");
+    });
 });
